38bd15a8ad767a1158b665beb84367170b8682ea
Shohjahon <104879567+mr-webprogrammer@users.noreply.github.com>
2022-11-02 16:19:14 +0500

    Add Navbar Header About Pages

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client'; 
+import ReactDOM from 'react-dom/client';
+
+// import main component 
 import App from './App';
+
+// import scss 
+
+// import css
+import "./css/style.css";
+
+// import boxicons
+import "boxicons/css/boxicons.min.css"
+
+// import BrowserRouter 
 import { BrowserRouter } from 'react-router-dom';
 
 
