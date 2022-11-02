e4583168d324bb75328522c3a3afbc81696046e4
Halimjon <khalimjonakbaron@gmail.com>
2022-11-02 15:53:43 +0500

    testimonials section is in progress

--- src/components/Khalim/testimonials/Testimonials.js
+++ src/components/Khalim/testimonials/Testimonials.js
@@ -3,11 +3,29 @@ import './testimonials.css'
 
 const Testimonials = () => {
   return (
-    <>
-      <div>
-        <h3 className='testiomonials-title'>Our Testimonials</h3>
+    <section className='testimonials-container'>
+      <div className='section-testimonials__title'>
+        <h3 className='testiomonials-title'>Отзывы клиентов</h3>
       </div>
-    </>
+      <div className='what-they-think'>
+        <div className='testimonials-items'>
+          <h4>Как же удебно</h4>
+          <p>B рамках спецификации современных стандартов, базовые 
+            сценарии поведения пользователей, которые представляют 
+            собой яркий пример континентально-европейского 
+            типа политической культуры, будут преданы социально-демократической анафеме.
+          </p>
+        </div>
+        <div className='testimonials-items'>
+          <h4>Теперь мы идем в ногу co временем</h4>
+          <p>A ещё интерактивные прототипы превращены 
+            в посмешище, хотя само их существование приносит 
+            несомненную пользу обществу. Банальные, но неопровержимые выводы, 
+            a также тщательные исследования конкурентов формируют сеть.
+          </p>
+        </div>
+      </div>
+    </section>
   )
 }
 
